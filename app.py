51a6eed64257b50bb1562c110fd8a30612a78f1f
NaorOrZion <naor5506@gmail.com>
2023-10-20 14:28:40 +0300

    update app.py, update client_secret.json

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
-"""
-Author: Naor Or-Zion
-Unit:   Basmach-Alpha
-Date:   19.10.2023
-
-Brief: The 'app.py' is the main navigation file for the website
-"""
-
 from flask import Flask, render_template
+from googleapiclient.discovery import build
+import google.oauth2.service_account
 
+# Consts
+FORM_ID = "1M1WV-FrjumLRmCSOGmXlN1bQ9A3agS9Znt-pdCzT9pk"
+SCOPE = "https://www.googleapis.com/auth/forms.responses.readonly"
+CLIENT_SECRET_PATH = "resources/client_secret.json"
+
+# App Initializer
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "pc-status"
 
-
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("home.html")
+    try:
+        # Authenticate with service account credentials
+        credentials = google.oauth2.service_account.Credentials.from_service_account_file(
+            CLIENT_SECRET_PATH,
+            scopes=SCOPE
+        )
 
+        # Build the Forms service
+        service = build('forms', 'v1', credentials=credentials)
+
+        # Retrieve form responses
+        response = service.forms().responses().list(formId=FORM_ID).execute()
+
+        # Process response data
+        responses = response.get('responses', [])
+        print(responses)
+        return render_template("home.html")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise
 
 if __name__ == "__main__":
     app.run(debug=True)
