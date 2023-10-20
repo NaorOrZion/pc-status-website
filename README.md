9201b07de2c7886aa4014dc85f9fd36b2d01a906
NaorOrZion <naor5506@gmail.com>
2023-10-20 21:37:46 +0300

    update app.py - successfully integrated with the API. modify README.md. add requirements.txt. modify home.html.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # pc-status-website
-This website is designed to track after the computers' status at the lab  
+This website is designed to track the computers' status at the lab.
+
+## How to use and initialize the website?
+
+#### Prerequities
+
+-   Python 3
+
+#### Installation
+
+1. Clone the github project to a designated folder in your desktop.
+```sh
+git clone https://github.com/NaorOrZion/pc-status-website
+```
+
+2. Move to the cloned folder.
+```sh
+cd pc-status-website
+```
+    
+3. Create a Virtual environment and activate it.
+```sh
+python -m venv venv
+venv\Scripts\activate
+```
+
+4. Install the requirements.
+```sh
+pip install -r requirements.txt
+```
+
+5. Run the main file (Notice that it will run on 127.0.0.1:5000 at it currently in development).
+```sh
+python main.py
+```
--- app.py
+++ app.py
@@ -1,39 +1,61 @@
 from flask import Flask, render_template
 from googleapiclient.discovery import build
 import google.oauth2.service_account
+from typing import Union, List, Dict
 
 # Consts
-FORM_ID = "1M1WV-FrjumLRmCSOGmXlN1bQ9A3agS9Znt-pdCzT9pk"
-SCOPE = "https://www.googleapis.com/auth/forms.responses.readonly"
+SCOPES = ["https://www.googleapis.com/auth/forms.responses.readonly"]
 CLIENT_SECRET_PATH = "resources/client_secret.json"
+
+# The form ID can be found in the edit mode of any form that was 
+# created under the user which made the Google Cloud Console project.
+FORM_ID = "1spTaWVM6t2BPGFGhmrfFszV2HZdgjzrSHzbPROny0wg"
+
+# Special Typing Types
+JsonType = List[Dict[Union[str, Dict[str, Dict[str, Union[str, Dict[str, List[Dict[str, str]]]]]]], str]]
 
 # App Initializer
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "pc-status"
 
-@app.route("/", methods=["GET"])
-def home():
-    try:
-        # Authenticate with service account credentials
-        credentials = google.oauth2.service_account.Credentials.from_service_account_file(
-            CLIENT_SECRET_PATH,
-            scopes=SCOPE
+
+def get_json_response() -> JsonType:
+    '''
+    This function gets the forms responses in a json format and returns it.
+
+    @params: None.
+    Returns: The form's answers in json type.
+    '''
+    # Authenticate with service account credentials
+    credentials = (
+        google.oauth2.service_account.Credentials.from_service_account_file(
+            CLIENT_SECRET_PATH, scopes=SCOPES
         )
+    )
 
-        # Build the Forms service
-        service = build('forms', 'v1', credentials=credentials)
+    # Build the Forms service
+    service = build("forms", "v1", credentials=credentials)
 
-        # Retrieve form responses
-        response = service.forms().responses().list(formId=FORM_ID).execute()
+    # Retrieve form responses
+    response = service.forms().responses().list(formId=FORM_ID).execute()
 
-        # Process response data
-        responses = response.get('responses', [])
-        print(responses)
-        return render_template("home.html")
+    # Process response data
+    responses = response.get("responses", [])
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        raise
+    return responses
+
+
+@app.route("/", methods=["GET"])
+def home_page():
+    '''
+    This function is responsible to render the main page
+
+    @params: None.
+    Returns: None.
+    '''
+    responses = get_json_response()
+    return render_template("home.html", responses=responses)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
